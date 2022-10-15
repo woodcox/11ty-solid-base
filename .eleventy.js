6085446acbebd14e6af2c7e052e1c506c1d12998
Paul <64870518+woodcock3@users.noreply.github.com>
2022-10-15 12:55:28 +0100

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,22 @@
+const sass = require("sass");
+
+const TEMPLATE_ENGINE = "liquid";
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget("./src/sass/");
+
+  // Let Eleventy transform HTML files as liquidjs
+  // So that we can use .html instead of .liquid
+  // 11ty.js template format also picks up on the esbuild.11ty.js script
 
   return {
     dir: {
       input: "src",
-      output: "public",
+      output: "docs",
+      data: "_data",
     },
+    templateFormats: ["html", "md", "11ty.js", TEMPLATE_ENGINE],
+    markdownTemplateEngine: TEMPLATE_ENGINE,
+    htmlTemplateEngine: TEMPLATE_ENGINE,
   };
 };
