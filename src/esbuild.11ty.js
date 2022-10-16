54de7c518049e00702d2a8fa426153862524bf77
Paul <64870518+woodcock3@users.noreply.github.com>
2022-10-16 04:26:13 +0100

    Update esbuild.11ty.js

--- src/esbuild.11ty.js
+++ src/esbuild.11ty.js
@@ -2,6 +2,7 @@ const esbuild = require('esbuild')
 const glob = require('glob-all') // to enable * glob pattern in esbuild
 const isProd = process.env.ELEVENTY_ENV === 'prod' ? true : false
 const solid = require('solid-js')
+const babel = require('esbuild-plugin-babel')
 
 module.exports = class {
   data() {
@@ -15,10 +16,12 @@ module.exports = class {
     await esbuild.buildSync({
       entryPoints: glob.sync(['assets/js/*.js']),
       bundle: true,
+      plugins: [babel()],
       minify: isProd,
       outdir: './docs/assets/js',
       sourcemap: !isProd,
       target: isProd ? 'es6' : 'esnext'
     })
+  .catch(() => process.exit(1));
   }
 }
