12a5f4cc001dfdde9ae0d62feda37f41eced0ebf
Paul <64870518+woodcock3@users.noreply.github.com>
2022-10-18 03:56:11 +0100

    Update esbuild.11ty.js

--- src/esbuild.11ty.js
+++ src/esbuild.11ty.js
@@ -13,7 +13,7 @@ module.exports = class {
 
   async render() {
     await esbuild.build({
-      entryPoints: glob.sync(['app/*.jsx']),
+      entryPoints: glob.sync(['src/app/*.jsx']),
       bundle: true,
       plugins: [solidPlugin()],
       minify: isProd,
