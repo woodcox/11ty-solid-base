492fe59663065b93255b4c6d55a48bba7f1db3ec
Paul <64870518+woodcock3@users.noreply.github.com>
2022-10-18 03:53:53 +0100

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,12 @@ const sass = require("sass");
 const TEMPLATE_ENGINE = "liquid";
 
 module.exports = function (eleventyConfig) {
+  // WATCH the scss files
   eleventyConfig.addWatchTarget("./src/sass/");
+  eleventyConfig.addPassthroughCopy({ './_tmp': './assets/css' });
+  
+  // WATCH the js files for esbuild in scripts.11ty.js
+  eleventyConfig.addWatchTarget('./src/app');
 
   // Let Eleventy transform HTML files as liquidjs
   // So that we can use .html instead of .liquid
