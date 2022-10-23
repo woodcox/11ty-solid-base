517eedd879983aac6598b02ef0eb2db74040c1fd
Paul <64870518+woodcock3@users.noreply.github.com>
2022-10-23 10:15:05 +0100

    Update solidify.js

--- config/shortcodes/solidify.js
+++ config/shortcodes/solidify.js
@@ -1,4 +1,4 @@
-const { transformSync } = require("esbuild");
+const esbuild = require("esbuild");
 const isProd = process.env.ELEVENTY_ENV === 'prod' ? true : false
 const { solidPlugin } = require('esbuild-plugin-solid')
 const solid = require('solid-js')
@@ -6,7 +6,7 @@ const solid = require('solid-js')
 
 // Note: transform will not bundle!
 module.exports = (content) => {
-  const result = transform(content, { 
+  const result = esbuild.transformSync(content, { 
     // loader: "jsx",
     // jsx: "preserve",
     // jsxImportSource: "solid-js",
