029f8e704d9ac9fb4550b8ab195dab1a874bead2
Paul <64870518+woodcock3@users.noreply.github.com>
2022-10-23 04:18:12 +0100

    Update insertsolid.js

--- src/shortcodes/insertsolid.js
+++ src/shortcodes/insertsolid.js
@@ -1,12 +1,15 @@
 const { transformSync } = require("esbuild");
 const isProd = process.env.ELEVENTY_ENV === 'prod' ? true : false
 const { solidPlugin } = require('esbuild-plugin-solid')
+const solid = require('solid-js')
 
 
 // Note: transform will not bundle!
 module.exports = (content) => {
   const result = transformSync(content, { 
-    loader: "js",
+    loader: "jsx",
+    jsx: "preserve",
+    "jsxImportSource": "solid",
     plugins: [solidPlugin()],
     minify: isProd
   });
