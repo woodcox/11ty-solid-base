ade07aaecf11b2c9d9d65d3ba69bb844e6ac07b8
Paul <64870518+woodcock3@users.noreply.github.com>
2022-10-24 10:08:11 +0100

    Update solidify.js

--- config/shortcodes/solidify.js
+++ config/shortcodes/solidify.js
@@ -1,18 +1,18 @@
 const esbuild = require("esbuild");
 const isProd = process.env.ELEVENTY_ENV === 'prod' ? true : false
 const { solidPlugin } = require('esbuild-plugin-solid');
-const fs = require('fs');
+const fsPromises = require('fs').promises;
 
 
 // Note: transform will not bundle!
 module.exports = async (code) => {
-  fs.writeFileSync('in.jsx', code),
+  await fsPromises.writeFile('in.jsx', code),
   await esbuild.buildSync({ 
     entryPoints: ['in.jsx'],
     outfile: 'out.js',
     plugins: [solidPlugin()],
     minify: isProd
   })
-  const bundle = fs.readFileSync('out.js', 'utf8')
+  const bundle = await fsPromises.readFile('out.js', 'utf8')
   return await bundle 
 };
