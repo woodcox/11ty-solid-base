f7a971176011f866cca09db61403231b79a136a0
Paul <64870518+woodcock3@users.noreply.github.com>
2022-10-24 03:43:08 +0100

    Update solidify.js

--- config/shortcodes/solidify.js
+++ config/shortcodes/solidify.js
@@ -13,8 +13,8 @@ module.exports = (content) => {
       plugins: [solidPlugin()],
       minify: isProd
     });
-  if (result) {
-    return `<script>${result}</script>`;
+  if (result.js) {
+    return `<script>${result.js}</script>`;
   }
   return `<script>console.log(${JSON.stringify(result.errors)})</script>`;
 };
