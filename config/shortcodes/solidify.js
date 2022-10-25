27ec501379956bb640c00c8a8c94fd3b892dc277
Paul <64870518+woodcock3@users.noreply.github.com>
2022-10-25 21:38:43 +0100

    Update solidify.js

--- config/shortcodes/solidify.js
+++ config/shortcodes/solidify.js
@@ -15,7 +15,7 @@ module.exports = async (code) => {
   })
   try {
     const solidifyJsx = await fsPromises.readFile('./docs/out.js', 'utf8');
-    return isProd ? 'await fsPromise.appendFile(${solidifyJsx}', 'utf8');' : '`<script type="module">${solidifyJsx}</script>`';
+    return isProd ? 'await fsPromise.appendFile(${solidifyJsx}, 'utf8');' : '`<script type="module">${solidifyJsx}</script>`';
   } catch(err) {
     console.log('${solidifyJsx}', err);
   }
