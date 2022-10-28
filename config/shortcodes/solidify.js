c48ea302c8b823e64713e70c1d683cc38259a65a
Paul <64870518+woodcock3@users.noreply.github.com>
2022-10-28 05:16:45 +0100

    Update solidify.js

--- config/shortcodes/solidify.js
+++ config/shortcodes/solidify.js
@@ -3,20 +3,21 @@ const isProd = process.env.ELEVENTY_ENV === 'prod' ? true : false
 const { solidPlugin } = require('esbuild-plugin-solid');
 const fsPromises = require('fs').promises;
 
-module.exports = async (code, outname, inline) => {
+module.exports = async (code, output, inline) => {
   await fsPromises.writeFile('src/in.jsx', code),
   await esbuild.build({ 
     entryPoints: ['src/in.jsx'],
-    outfile: './docs/out${outname}.js',
+    outdir: './docs',
+    outExtension: {'.js': ${output}'.js'},
     bundle: true,
     plugins: [solidPlugin()],
     minify: isProd,
     target: isProd ? 'es6' : 'esnext'
   })
   try {
-    const solidifyJsx = await fsPromises.readFile('./docs/out${extension}.js', 'utf8');
+    const solidifyJsx = await fsPromises.readFile('./docs/' + ${output} + '.js', 'utf8');
     return `<script type="module">${solidifyJsx}</script>`;
   } catch(err) {
-    console.log(' + solidifyJsx + ', err);
+    console.log('Solidify Jsx Shortcode', err);
   }
 };
