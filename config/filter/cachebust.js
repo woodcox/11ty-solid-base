4408a9dc9cf161ed1845366fe058014287df17da
Paul <64870518+woodcock3@users.noreply.github.com>
2022-10-29 05:06:06 +0100

    Create cachebust.js

---
+++ config/filter/cachebust.js
@@ -0,0 +1,6 @@
+eleventyConfig.addFilter("bust", (url) => {
+    const [urlPart, paramPart] = url.split("?");
+    const params = new URLSearchParams(paramPart || "");
+    params.set("v", `${now}`);
+    return `${urlPart}?${params}`;
+  });
