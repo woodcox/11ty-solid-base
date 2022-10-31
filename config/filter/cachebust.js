3027e33d835da039b9e84cb91057a9df77daef5b
Paul <64870518+woodcock3@users.noreply.github.com>
2022-10-31 22:51:02 +0000

    Update cachebust.js

--- config/filter/cachebust.js
+++ config/filter/cachebust.js
@@ -23,38 +23,3 @@ console.log(readDataFile);
 
 
 
-
-readCssFile()
-  .then((data) => {
-    hashCss = md5(data).slice(0, 16)
-    console.log(hashCss)
-  })
-  .then(() => {
-    readJsFile()
-      .then((data) => {
-        hashJs = md5(data).slice(0, 16)
-        console.log(hashJs)
-      })
-      .then(() => {
-        const versionObject = {
-          css: hashCss,
-          js: hashJs
-        }
-
-        writeFile('src/_data/version.json', JSON.stringify(versionObject), (err) => {
-          if (err) throw err
-          console.log('The file has been saved!')
-        })
-      })
-      .then(() => {
-        fs.rename('dist/styles.css', `dist/styles${hashCss}.css`, function(err) {
-          if ( err ) return console.log('ERROR: ' + err)
-          console.log(`dist/styles.css > dist/styles${hashCss}.css`)
-        })
-
-        fs.rename('dist/index.js', `dist/index${hashJs}.js`, function(err) {
-          if ( err ) return console.log('ERROR: ' + err)
-          console.log(`dist/index.js > dist/index${hashJs}.js`)
-        })
-      })
-  })
