d4f50dfc092aa000092fe3d41e59a519d55cdf7d
Paul <64870518+woodcock3@users.noreply.github.com>
2022-11-04 00:38:02 +0000

    Update cachebust.js

--- config/filter/cachebust.js
+++ config/filter/cachebust.js
@@ -19,18 +19,18 @@ module.exports = (url) => {
   console.log(url);
 };
 
-const readDataFile = () => {
-  fs.readFile('src/_data/buildmeta.json', (err, data) => {
-    if (err) throw err;
-    let buildmeta = JSON.parse(data);
-    if (err) console.log(err);
-    console.log(buildmeta);
-  })
-  if (url === buildmeta.outputs[0].input ) { 
-    url = buildmeta.outputs[0]
-  }
-  console.log(buildmeta.outputs[0]);
-}
+// const readDataFile = () => {
+//  fs.readFile('src/_data/buildmeta.json', (err, data) => {
+//    if (err) throw err;
+//    let buildmeta = JSON.parse(data);
+//    if (err) console.log(err);
+//    console.log(buildmeta);
+//  })
+//  if (url === buildmeta.outputs[0].input ) { 
+//    url = buildmeta.outputs[0]
+//  }
+//  console.log(buildmeta.outputs[0]);
+// }
 
 //fs.readFile('src/_data/buildmeta.json', (err, data) => {
 //  if (err) throw err;
@@ -41,19 +41,19 @@ const readDataFile = () => {
 //  if (err) console.log(err);
 //});
 
-fs.readFile('src/_data/buildmeta.json', (err, data) => {
-  if (err) throw err;
-  let esbuildmeta = JSON.parse(data);
-  if (err) console.log(err);
+// fs.readFile('src/_data/buildmeta.json', (err, data) => {
+//   if (err) throw err;
+//   let esbuildmeta = JSON.parse(data);
+//  if (err) console.log(err);
   // https://medium.com/@prathameshk73/get-nested-properties-in-javascript-objects-97a9b1b0750f
-  function getNestedObject(obj, key) {
-    return key.split(".").reduce(function(o, x) {
-      return (typeof o == "undefined" || o === null)? o: o[x]
-    }, obj);
-    getNestedObject(data, 'outputs[0]'); 
-  }
-  console.log(getNestedObject);
-});
+//  function getNestedObject(obj, key) {
+//    return key.split(".").reduce(function(o, x) {
+//      return (typeof o == "undefined" || o === null)? o: o[x]
+//    }, obj);
+//    getNestedObject(data, 'outputs[0]'); 
+//  }
+//  console.log(getNestedObject);
+//});
 
 
 
