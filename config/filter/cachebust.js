ef9294d1c28435879b86772658f326fe91634834
Paul <64870518+woodcock3@users.noreply.github.com>
2022-11-04 00:24:35 +0000

    Update cachebust.js

--- config/filter/cachebust.js
+++ config/filter/cachebust.js
@@ -6,10 +6,17 @@ let hashJs;
 
 
 module.exports = (url) => {
-  const [urlPart, paramPart] = url.split("?");
-  const params = new URLSearchParams(paramPart || "");
-  params.set("v", `${now}`);
-  return `${urlPart}?${params}`;
+  fs.readFile('src/_data/buildmeta.json', (err, data) => {
+    if (err) throw err;
+    let buildmeta = JSON.parse(data);
+    if (err) console.log(err);
+    console.log(buildmeta);
+  })
+  if (url === buildmeta.outputs[0].input ) { 
+    url = buildmeta.outputs[0]
+  }
+  return `${url}`;
+  console.log(${url});
 };
 
 const readDataFile = () => {
