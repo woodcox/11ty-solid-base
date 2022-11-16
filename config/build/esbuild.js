f9afe4e411fc2b843525ff235e180c27e88a1932
Paul <64870518+woodcock3@users.noreply.github.com>
2022-11-16 04:44:11 +0000

    Update esbuild.js

--- config/build/esbuild.js
+++ config/build/esbuild.js
@@ -8,7 +8,7 @@ const fs = require('fs');
 module.exports = async () => {
   result = await esbuild.build({
     entryPoints: glob.sync(['src/assets/app/*.jsx', 'src/assets/js/*.js']),
-    entryNames: '[dir]/[name]-[hash]',
+    entryNames: '[dir]/[name]',
     outExtension: isProd ? {'.js': '.min.js', '.css': '.min.css'} : {'.js': '.js', '.css': '.css'},
     bundle: true,
     plugins: [solidPlugin()],
