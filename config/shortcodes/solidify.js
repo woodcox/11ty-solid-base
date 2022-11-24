4e4848c3ee90c1368e64eb23ab00eae884170474
Paul <64870518+woodcock3@users.noreply.github.com>
2022-11-24 04:09:51 +0000

    Update solidify.js

--- config/shortcodes/solidify.js
+++ config/shortcodes/solidify.js
@@ -8,10 +8,12 @@ const { http, default_schemes } = require('@hyrious/esbuild-plugin-http');
 module.exports = async (code, filename, bundled) => {
   let bundleJsx = bundled !== 'bundleOff' ? true : false;
   await fsPromises.writeFile('solid-' + filename + '.jsx', code),
+  
+  // esm version
   await esbuild.build({
     entryPoints: glob.sync(['solid-*.jsx']),
     entryNames: '[name]',
-    outdir: './docs/assets/app',
+    outdir: './docs/assets/app/esm ',
     bundle: bundleJsx,
     format: 'esm',
     plugins: [
@@ -24,7 +26,27 @@ module.exports = async (code, filename, bundled) => {
     ],
     minify: isProd,
     target: isProd ? 'es6' : 'esnext'
+  }),
+
+  // Cjs version
+  await esbuild.build({
+    entryPoints: glob.sync(['solid-*.jsx']),
+    entryNames: '[name]',
+    outdir: './docs/assets/app/cjs',
+    bundle: bundleJsx,
+    format: 'cjs',
+    plugins: [
+      solidPlugin(),
+      http({
+        filter: (url) => true,
+        schemes: { default_schemes },
+        cache: new Map()
+      })
+    ],
+    minify: isProd,
+    target: isProd ? 'es6' : 'esnext'
   })
-  const solidifyJsx = await fsPromises.readFile('./docs/assets/app/solid-' + filename + '.js', 'utf8');
-  return `<script type="module">${solidifyJsx}</script>`;
+  const solidifyEsm= await fsPromises.readFile('./docs/assets/app/esm/solid-' + filename + '.js', 'utf8');
+  const solidifyCjs= await fsPromises.readFile('./docs/assets/app/cjs/solid-' + filename + '.js', 'utf8');
+  return `<script type="module">${solidifyEsm}</script><script nomodule>${solidifyCjs}</script>`;
 };
