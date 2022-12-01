31b22e3e22ef0808b0dbf48740cff785b12af0cd
woodcock3 <64870518+woodcock3@users.noreply.github.com>
2022-12-01 18:29:03 +0000

    7 files modified, 5 added, 2 deleted

---
+++ solid-shorty.jsx
@@ -0,0 +1,8 @@
+
+import { render } from 'https://esm.sh/solid-js/web';
+
+function Solidify() {
+  return <div>The solidify shortcode is active!</div>;
+}
+
+render(() => <Solidify />, document.getElementById('shorty'))
