5279cef30c6ab72249e558deb2c81464a6cdbc92
Paul <64870518+woodcock3@users.noreply.github.com>
2022-12-02 16:51:31 +0000

    Delete solid-shorty.jsx

--- solid-shorty.jsx
+++
@@ -1,8 +0,0 @@
-
-import { render } from 'https://esm.sh/solid-js/web';
-
-function Solidify() {
-  return <div>The solidify shortcode is active!</div>;
-}
-
-render(() => <Solidify />, document.getElementById('shorty'))
