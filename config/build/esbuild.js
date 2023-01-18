d036a22880a6837949d62e44d49ba87b8042b7e1
Paul <64870518+woodcox@users.noreply.github.com>
2023-01-18 22:40:02 +0000

    Update esbuild.js

--- config/build/esbuild.js
+++ config/build/esbuild.js
@@ -9,31 +9,33 @@ const purgecssPlugin2 = require("esbuild-plugin-purgecss-2");
 const fs = require('fs');
 const path = require("path");
 
-let purgecssPlugin = {
-  name: 'purgecss',
-  setup(build) {
-    if (!build.initialOptions.metafile) {
-      throw new Error('You should set metafile true to use this plugin.');
-    }
-    const { PurgeCSS } = require('@fullhuman/postcss-purgecss');
-    const path = require('path');
-    const fs = require('fs');
-
-    build.onEnd(async (args) => {
-      const outputKeys = Object.keys(args.metafile.outputs);
-      const genFilter = (postfix) => (k) => k.endsWith(postfix);
-
-      const css = outputKeys.filter(genFilter('.css'));
-      const opts = options ? options : {};
-
-      const res = await new PurgeCSS().purge({ ...opts, css: css });
-
-      for (let index = 0; index < res.length; index++) {
-        const { file, css } = res[index];
-        await fs.promises.writeFile(file, css);
+let purgecssPlugin = function purgecssPlugin(options) {
+  return {
+    name: 'purgecss',
+    setup(build) {
+      if (!build.initialOptions.metafile) {
+        throw new Error('You should set metafile true to use this plugin.');
       }
-    });
-  },
+      const { PurgeCSS } = require('@fullhuman/postcss-purgecss');
+      const path = require('path');
+      const fs = require('fs');
+
+      build.onEnd(async (args) => {
+        const outputKeys = Object.keys(args.metafile.outputs);
+        const genFilter = (postfix) => (k) => k.endsWith(postfix);
+
+        const css = outputKeys.filter(genFilter('.css'));
+        const opts = options ? options : {};
+
+        const res = await new PurgeCSS().purge({ ...opts, css: css });
+
+        for (let index = 0; index < res.length; index++) {
+          const { file, css } = res[index];
+          await fs.promises.writeFile(file, css);
+        }
+      });
+    },
+  };
 };
 
 
