8934778dc35ca298e3404b8598d2d70372e92c85
Paul <64870518+woodcox@users.noreply.github.com>
2023-01-18 16:39:38 +0000

    Update esbuild.js

--- config/build/esbuild.js
+++ config/build/esbuild.js
@@ -29,7 +29,7 @@ module.exports = async () => {
         cache: new Map()
       }),
       purgecssPlugin2({
-        content: ["index.html"],
+        content: ["./src/index.md"],
         css:['style.css']
       }),
       solidPlugin(), 
