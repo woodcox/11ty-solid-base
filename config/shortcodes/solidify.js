f45a612d65f33ebcf88d5f2f7317fd0a314180d0
Paul <64870518+woodcox@users.noreply.github.com>
2023-01-22 00:01:35 +0000

    Update solidify.js

--- config/shortcodes/solidify.js
+++ config/shortcodes/solidify.js
@@ -10,7 +10,7 @@ module.exports = async (code, filename, bundled) => {
   await fsPromises.writeFile('./_tmp/solid-' + filename + '.jsx', code),
   
   // esm version
-  await esbuild.build({
+  let res = await esbuild.build({
     entryPoints: glob.sync(['_tmp/solid-*.jsx']),
     entryNames: '[name]',
     // write: false,
@@ -31,6 +31,9 @@ module.exports = async (code, filename, bundled) => {
     console.error(err);
     process.exitCode = 1;
   })
+  for (let out of res.outputFiles) {
+    console.log(out.path, out.contents, out.text)
+  }
   const solidifyJsx= await fsPromises.readFile('./docs/app/solid-' + filename + '.js', 'utf8');
   return `<script type="module">${solidifyJsx}</script>`;
 };
