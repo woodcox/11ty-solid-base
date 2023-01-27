6598044a8d48b42bd20cb3f2c00372e9f9485288
Paul <64870518+woodcox@users.noreply.github.com>
2023-01-27 20:59:20 +0000

    Update esbuild.js

--- config/build/esbuild.js
+++ config/build/esbuild.js
@@ -30,7 +30,7 @@ module.exports = async () => {
       }),
       purgecssPlugin({
         // assumes production build is on github pages. You may want ["dist/index.html"]
-        content: ["index.html"]
+        content: ["./index.html"]
       }),
       solidPlugin(), 
       manifestPlugin({
