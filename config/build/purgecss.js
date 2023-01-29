655ec9bebb9311456d692dd245ceddce760c8d1c
Paul <64870518+woodcox@users.noreply.github.com>
2023-01-29 01:20:57 +0000

    Update purgecss.js

--- config/build/purgecss.js
+++ config/build/purgecss.js
@@ -1,7 +1,48 @@
 // A modified version of https://github.com/arslanakram/esbuild-plugin-purgecss-2.0
 const esbuild = require('esbuild');
 const glob = require('glob-all');
-const purgecssPlugin = require('./purgecssPlugin.js');
+const fs = require('fs');
+const { PurgeCSS } = require('purgecss');
+const path = require('path');
+const buildMetafile = JSON.parse(fs.readFileSync('./src/_data/buildmeta.json', 'utf8'));
+
+let purgecssPlugin = function purgecssPlugin(options) {
+  return {
+    name: 'purgecss',
+    setup(build) {
+      if (!buildMetafile) {
+        throw new Error('Make sure eleventy generates a buildmeta.json from .src/config/build/esbuild.js');
+      }
+
+      build.onEnd(async (args) => {
+        path: args.path;
+        // outputKeyss gets metafile build output of .js files and .css files
+        const outputKeys = Object.keys(buildMetafile.outputs);
+        console.log(outputKeys);
+        // create a file extension filter 
+        const genFilter = (postfix) => (k) => k.endsWith(postfix);
+        // filter the metafile output to only return the css files
+        const css = outputKeys.filter(genFilter('.css'));
+      
+        // Create a jS object of the purgecss config for css
+        let cssConfig = { css: css };
+        // Merge the css config with the purgecssPlugin options
+        let config = Object.assign(options, cssConfig);
+
+        // check if there is the purgecss config js object and throw error if not
+        const opts = config ? config : () => {};
+        
+        // pass the purgecss config including the relevant css file to purgecss
+        const purgeResult = await new PurgeCSS().purge({ ...opts });
+
+        for (let index = 0; index < purgeResult.length; index++) {
+          const { file, css } = purgeResult[index];
+          await fs.promises.writeFile(file, css);
+        }
+      });
+    },
+  };
+};
 
 
 module.exports = async () => {
